191e78d65b67e21e3664a5e1c33cb289d83a348b
rptaszek <rptaszek.gmail.com>
2018-10-12 11:21:17 +0200

    wstawianie znaków niepełny

--- getc_putchar.c
+++ getc_putchar.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 int main (){
     char znak;
-    printf("wciśnij przycisk i zatwierdź \n");
-    znak = getchar();
-    putchar(znak);
+    int wartosc_calkowita;
+    double wartosc_wymierna;
+    char ciag_znakow[256];
+
+    printf("Podaj znak: ");
+    scanf("%c", &znak);
+    printf("Podaj ciąg znakowy");
+    scanf("%s", ciag_znakow);
+    printf("Podaj liczbę całkowitą: ");
+    scanf("%i", &wartosc_calkowita);
+    printf("Podaj liczbę wymierną: ");
+    scanf("%lf", &wartosc_wymierna);
+    {
+        int f;
+        f = 10;
+        printf("%i", f);
+    }
+    printf("\n");
+    printf("znak = %c, \nciąg znakowy)liczba całkowita = %i,"
+           "\n liczba wymierna = %lf",
+           znak,
+           wartosc_calkowita,
+           wartosc_wymierna
+           );
+
+
     return 0;
 
 }
